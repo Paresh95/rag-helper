44acd010e57f1763479877f51712e53c18e23314
Paresh Sharma <pareshs@hotmail.co.uk>
2026-02-06 10:42:01 +0000

    improve litellm function

--- src/rag_main.py
+++ src/rag_main.py
@@ -1,11 +1,11 @@
 from typing import List
-
+import asyncio
 from src.utils.schemas import RAGResponse
 from src.utils.schemas import RetrievedChunk
 from src.online_components.retriever import SimpleHybridRetriever
 from src.online_components.reranker import SentenceTransformerReranker
 from src.online_components.generator import Generator
-from src.prompts.v1_prompt import USER_PROMPT_TEMPLATE
+from src.prompts.v1_prompt import SYSTEM_PROMPT, USER_PROMPT_TEMPLATE
 from llama_index.core.vector_stores import MetadataFilters, ExactMatchFilter
 
 
@@ -25,11 +25,12 @@ class VanillaRAG:
             chunks = self.reranker.rerank(query, chunks)
         return chunks
 
-    def generate_answer(self, query: str, chunks: List[RetrievedChunk]) -> str:
-        return self.generator.generate(query, chunks)
+    async def generate_answer(self, query: str, chunks: List[RetrievedChunk]) -> str:
+        return await self.generator.generate(query, chunks)
 
-    def run(self, retriever_query: str, generator_query: str) -> RAGResponse:
+    async def run(self, retriever_query: str, generator_query: str) -> RAGResponse:
         nodes = self.retrieve(retriever_query)
+
         chunks: List[RetrievedChunk] = [
             RetrievedChunk(
                 text=n.node.get_content(),
@@ -38,13 +39,13 @@ class VanillaRAG:
             )
             for n in nodes
         ]
-        answer = self.generate_answer(generator_query, chunks)
+
+        answer = await self.generate_answer(generator_query, chunks)
+
         return RAGResponse(
             answer=answer,
             retrieved_chunks=chunks,
-            metadata={
-                "num_contexts": len(chunks),
-            },
+            metadata={"num_contexts": len(chunks)},
         )
 
 
@@ -57,14 +58,20 @@ if __name__ == "__main__":
             ),
         ]
     )
+
     retriever = SimpleHybridRetriever(
         similarity_top_k=10, sparse_top_k=10, hybrid_top_k=7, filters=filters
     )
+
     reranker = SentenceTransformerReranker(
-        model_name="cross-encoder/ms-marco-MiniLM-L-6-v2", top_n=3
+        model_name="cross-encoder/ms-marco-MiniLM-L-6-v2",
+        top_n=3,
     )
+
     generator = Generator(
-        model_name="gpt-4o-mini", prompt_template=USER_PROMPT_TEMPLATE
+        model_name="gpt-4o-mini",
+        system_prompt=SYSTEM_PROMPT,
+        user_prompt_template=USER_PROMPT_TEMPLATE,
     )
 
     retriever_query = (
@@ -73,5 +80,7 @@ if __name__ == "__main__":
     generator_query = "What does figure 2 show?"
 
     rag_pipeline = VanillaRAG(retriever, reranker, generator)
-    response = rag_pipeline.run(retriever_query, generator_query)
+
+    response = asyncio.run(rag_pipeline.run(retriever_query, generator_query))
+
     print(response)
